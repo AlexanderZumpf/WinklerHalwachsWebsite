9ddcc6ec46d8525a3b8ba20c1c9dcec901d42170
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-12 10:21:20 +0100

    Kragarm eingefügt

--- src/components/pages/kragarmstiegen-page/kragarmstiegen-page.component.ts
+++ src/components/pages/kragarmstiegen-page/kragarmstiegen-page.component.ts
@@ -7,4 +7,12 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KragarmstiegenPageComponent {}
+export class KragarmstiegenPageComponent {
+  pictureList = [
+    "1.jpeg", "2.jpg", "3.jpeg", "4.jpeg", 
+    "6.jpeg", "7.jpg", "8.jpg", "9.jpg", "10.jpg",
+    "11.jpg", "12.jpg", "13.jpeg", "14.jpg", "15.jpeg",
+    "16.jpeg", "18.jpeg", "19.jpg", "20.jpg",
+    "21.jpg", "22.jpg", "23.jpg", "24.jpg"
+  ];
+}
