f08a4d5d0b3aa456b9e1d075bc35e574751fd638
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-12 10:24:08 +0100

    Stahlstiegen eingefügt

--- src/components/pages/stahlstiegen-page/stahlstiegen-page.component.ts
+++ src/components/pages/stahlstiegen-page/stahlstiegen-page.component.ts
@@ -7,4 +7,13 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StahlstiegenPageComponent {}
+export class StahlstiegenPageComponent {
+  pictureList = [
+    "1.png", "2.jpg", "3.jpg", "4.jpeg", "5.jpg",
+    "6.jpeg", "7.jpeg", "8.jpg", "9.jpg", "10.jpg",
+    "11.jpg", "12.jpeg", "13.jpg", "14.jpeg", "15.jpeg",
+    "16.jpeg", "17.jpeg", "18.jpg", "19.jpg", "20.jpg",
+    "21.jpeg", "22.jpg", "23.jpg", "25.jpg",
+    "26.jpg", "27.jpg", "28.jpeg", "29.jpg"
+  ];
+}
