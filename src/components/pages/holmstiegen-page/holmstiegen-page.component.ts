37f6bcba1ef3434be06ccfb48b9bee28b624781b
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-12 10:16:47 +0100

    Faltwerk und Holmstiege eingefügt

--- src/components/pages/holmstiegen-page/holmstiegen-page.component.ts
+++ src/components/pages/holmstiegen-page/holmstiegen-page.component.ts
@@ -7,4 +7,11 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HolmstiegenPageComponent {}
+export class HolmstiegenPageComponent {
+  pictureList = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpeg",
+    "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg",
+    "11.jpg", "12.jpeg", "13.jpeg", "14.jpg", "15.jpg",
+    "16.jpg", "17.jpg", "18.jpg", "19.jpg"
+  ];
+}
