f6c28dc04ccbd3fcb5df8d29688895b0d6b1423c
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-12 09:53:12 +0100

    Glastüren eingefügt

--- src/components/pages/glastueren-page/glastueren-page.component.ts
+++ src/components/pages/glastueren-page/glastueren-page.component.ts
@@ -7,4 +7,13 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GlastuerenPageComponent {}
+export class GlastuerenPageComponent {
+  pictureList = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpeg", "5.jpg",
+    "6.jpg", "7.jpg", "8.jpeg", "9.jpg", "10.jpg",
+    "11.jpg", "12.jpg", "13.jpeg", "14.jpg", "15.jpg",
+    "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg",
+    "22.jpg", "23.jpeg", "24.jpg", "25.jpg"
+  ];
+}
+
