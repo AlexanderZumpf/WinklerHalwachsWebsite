0b14c3768dbbc9698641afae80027340aee97303
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-12 10:33:54 +0100

    Wendeltreppe eingefügt

--- src/components/pages/wendeltreppen-page/wendeltreppen-page.component.ts
+++ src/components/pages/wendeltreppen-page/wendeltreppen-page.component.ts
@@ -7,4 +7,11 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WendeltreppenPageComponent {}
+export class WendeltreppenPageComponent {
+  pictureList = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpeg", "5.jpeg",
+    "6.jpg", "7.jpg", "8.jpeg", "10.jpeg", "11.jpg",
+    "12.jpeg", "13.jpeg", "14.jpg", "15.jpg", "16.jpg",
+    "17.jpeg"
+  ];
+}
