cd6e075920b154ad200c4e5b521898d4ac931c49
SmartSquareAZ <alexander.zumpf@smartsquare.at>
2026-02-16 08:47:50 +0100

    Deploy Script eingefügt

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+APP_NAME="WinklerHalwachsWebsite"
+DEPLOY_PATH="/var/www/winkler-halwachs"
+
+echo "Load Angular app..."
+npm install
+
+echo "Building Angular app..."
+ng build --configuration production
+
+echo "Clearing old files..."
+sudo rm -rf $DEPLOY_PATH/*
+
+echo "Copying new build..."
+sudo cp -r dist/$APP_NAME/browser/* $DEPLOY_PATH/
+
+echo "Fixing permissions..."
+sudo chown -R www-data:www-data $DEPLOY_PATH
+
+echo "Reloading Nginx..."
+sudo systemctl reload nginx
+
+echo "Deployment complete."
